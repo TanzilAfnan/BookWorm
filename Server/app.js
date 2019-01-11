5cda3cbda0f4f6687f2f73b469aaf86c973a7ab8
tanzil_afn <tanzil.afn@hotmail.com>
2019-01-12 01:09:49 +0600

    Server without Mongodb

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,32 @@
 const express = require ('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
+    schema,
+    //Or we can also wtite the code below with es6
+    // schema
 
+    graphiql: true
 }));
 
 app.listen(4000, ()=>{
     console.log("Listening to port 4000");
 });
+// const express = require('express');
+// const graphqlHTTP = require('express-graphql');
+// const schema = require('./schema/schema');
+//
+// const app = express();
+//
+// // bind express with graphql
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: true
+// }));
+//
+// app.listen(4000, () => {
+//     console.log('now listening for requests on port 4000');
+// });
